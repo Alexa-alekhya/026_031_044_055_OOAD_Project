fe1ed010aea4557917ea9796082bed4c8f6594bd
aimenfathima <116288137+AiMeN0425@users.noreply.github.com>
2024-04-20 16:35:14 +0530

    Update PaymentRepository.java

--- src/main/java/tms/demo/repository/PaymentRepository.java
+++ src/main/java/tms/demo/repository/PaymentRepository.java
@@ -1,11 +1,9 @@
 package tms.demo.repository;
 
 public class PaymentRepository {
-    // Singleton implementation for PaymentRepository
     private static PaymentRepository instance;
 
     private PaymentRepository() {
-        // Private constructor to prevent instantiation from outside
     }
 
     public static PaymentRepository getInstance() {
@@ -14,8 +12,6 @@ public class PaymentRepository {
         }
         return instance;
     }
-
-    // Other fields and methods related to PaymentRepository class
 }
 
 
